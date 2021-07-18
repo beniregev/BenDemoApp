1ce1ae95bea4b1be881b2bdbb59f6dd000c2dc42
binyamin.regev <beniregev@gmail.com>
2021-07-18 08:22:36 +0300

    Initial Version - Step #0: application.properties configuration file and application Application class

---
+++ src/main/java/com/beniregev/den_demo_app/DenDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.beniregev.den_demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DenDemoAppApplication.class, args);
+    }
+
+}
